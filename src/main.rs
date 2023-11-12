8e5a78151a3a9c2181c8854eb071894bc3ba66ec
greidinger-reis <jp.greidinger@hotmail.com>
2023-11-12 19:38:10 -0300

    get random, get, get by abbr

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![allow(dead_code, unused_variables)]
 
 use quick_xml::de::{from_str, DeError};
-use serde::Deserialize;
+use rand::seq::SliceRandom;
+use serde::{Deserialize, Serialize};
 
 const FILE_STR: &'static str = include_str!("../bible-por-nvi.xml");
 
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Default, Deserialize )]
 #[serde(rename = "bible")]
 struct Bible {
     #[serde(rename = "book")]
@@ -18,6 +19,58 @@ impl Bible {
 
         Ok(bible)
     }
+
+    fn random(&self) -> BibleVerseResponse {
+        let book = self.books.choose(&mut rand::thread_rng()).unwrap();
+        let chapter = book.chapters.choose(&mut rand::thread_rng()).unwrap();
+        let verse = chapter.verses.choose(&mut rand::thread_rng()).unwrap();
+
+        BibleVerseResponse {
+            book: book.name.clone(),
+            chapter: chapter.number,
+            verse: verse.number,
+            content: verse.content.clone(),
+        }
+    }
+
+    fn get(&self, book_name: &str, chapter: usize, verse: usize) -> Option<BibleVerseResponse> {
+        let book = self.books.iter().find(|b| b.name == book_name)?;
+        let chapter = book.chapters.iter().find(|c| c.number == chapter)?;
+        let verse = chapter.verses.iter().find(|v| v.number == verse)?;
+
+        Some(BibleVerseResponse {
+            book: book.name.clone(),
+            chapter: chapter.number,
+            verse: verse.number,
+            content: verse.content.clone(),
+        })
+    }
+
+    fn get_by_abbr(
+        &self,
+        book_abbr: &str,
+        chapter: usize,
+        verse: usize,
+    ) -> Option<BibleVerseResponse> {
+        let book = self.books.iter().find(|b| b.abbrev == book_abbr)?;
+        let chapter = book.chapters.iter().find(|c| c.number == chapter)?;
+        let verse = chapter.verses.iter().find(|v| v.number == verse)?;
+
+        Some(BibleVerseResponse {
+            book: book.name.clone(),
+            chapter: chapter.number,
+            verse: verse.number,
+            content: verse.content.clone(),
+        })
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct BibleVerseResponse {
+    book: String,
+    chapter: usize,
+    verse: usize,
+    content: String,
 }
 
 #[derive(Debug, Default, Deserialize)]
@@ -27,7 +80,7 @@ struct BibleBook {
     #[serde(rename = "@abbrev")]
     abbrev: String,
     #[serde(rename = "@chapters")]
-    chapters_len: String,
+    chapters_len: usize,
 
     #[serde(rename = "c")]
     chapters: Vec<BibleBookChapter>,
@@ -36,7 +89,7 @@ struct BibleBook {
 #[derive(Debug, Default, Deserialize)]
 struct BibleBookChapter {
     #[serde(rename = "@n")]
-    number: String,
+    number: usize,
 
     #[serde(rename = "v")]
     verses: Vec<BibleBookVerse>,
@@ -45,15 +98,17 @@ struct BibleBookChapter {
 #[derive(Debug, Default, Deserialize)]
 struct BibleBookVerse {
     #[serde(rename = "@n")]
-    number: String,
+    number: usize,
     #[serde(rename = "$text")]
     content: String,
 }
 
-
 fn main() {
     let bible = Bible::from_xml().unwrap();
-    println!("Bible: {:?}", bible);
-
-    // println!("{:?}", bible.books[0].chapters[0].verses[0].content);
+    let rand_verse = bible.random();
+    let verse = bible.get("Gênesis", 1, 1).unwrap();
+    let verse_abbr = bible.get_by_abbr("gn", 1, 1).unwrap();
+    println!("Random Verse: {:?}", rand_verse);
+    println!("Verse: {:?}", verse);
+    println!("Verse by Abbr: {:?}", verse_abbr);
 }
